35b787ff9408599f413713c05d035076bb5fa783
Paweł <pawelpiatek98@gmail.com>
2018-10-21 21:28:23 +0200

    method example

---
+++ funkcje/funkcje/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funkcje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            test();
+            Console.ReadKey();
+        }
+        static void test()
+        {
+            Console.WriteLine("podaj słowo");
+           string word = Console.ReadLine();
+            Console.WriteLine("podaj ilość iteracji");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i <= number; i++)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
