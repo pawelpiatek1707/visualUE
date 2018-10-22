fa815dacbe6694e73bb0fa33fc3f30541cdd2b12
Paweł <pawelpiatek98@gmail.com>
2018-10-22 19:12:23 +0200

    niedokończone rysowanie

---
+++ Zadanie 3.9/Zadanie 3.9/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_3._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Menu-------------------------------
+            
+            Console.WriteLine("Opcje 1: ");
+            Console.WriteLine("*");
+            Console.WriteLine("**");
+            Console.WriteLine("***");
+            Console.WriteLine("****");
+            Console.WriteLine("---------");
+            Console.WriteLine("Opcja 2: ");
+            Console.WriteLine("****");
+            Console.WriteLine("***");
+            Console.WriteLine("**");
+            Console.WriteLine("*");
+            Console.WriteLine("---------");
+            Console.WriteLine("Opcja 3: ");
+            Console.WriteLine("   *");
+            Console.WriteLine("  **");
+            Console.WriteLine(" ***");
+            Console.WriteLine("****");
+            Console.WriteLine("---------");
+            Console.WriteLine("Opcja 4: ");
+            Console.WriteLine("*****");
+            Console.WriteLine("");
+            Console.WriteLine("*   *");
+            Console.WriteLine("");
+            Console.WriteLine("*   *");
+            Console.WriteLine("");
+            Console.WriteLine("*****");
+            Console.WriteLine("Wybierz opcje");
+            //------------------------------------
+            int choice = int.Parse(Console.ReadLine());
+            switch ( choice)
+            {
+                case 1:
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Podaj liczbe wierszy: ");
+                        int line = int.Parse(Console.ReadLine());
+                        string[,] arr = new string[line,line];
+                        string star = "*";
+                        for(int i = 0; i < line-1; i++)
+                        {
+                            for(int j=0; j< line-1; j++)
+                            {
+                                Console.WriteLine(arr[j, i] = star); 
+                            }
+                            Console.WriteLine();
+                        }
+
+                        break;
+                    }
+            }
+            Console.ReadKey(); 
+        }
+    }
+}
---
+++ Zadanie 3.9/Zadanie 3.9/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Zadanie 3.9")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Zadanie 3.9")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("49268d87-5cbc-4103-968c-4d992f2a7ceb")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
