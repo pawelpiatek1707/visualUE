073c22658214302e9fa409c1bfc8153b8c385fca
Paweł <pawelpiatek98@gmail.com>
2018-10-22 19:17:03 +0200

    zadanie 6.2

---
+++ zadanie 6.2/zadanie 6.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("wprowadz ilosc iteracji: ");
+            int it = int.Parse(Console.ReadLine());
+            Console.WriteLine("Iteracje petla do while: ");
+            int i = 0;
+            do
+            {
+                Console.WriteLine(++i);
+            } while (i <= it);
+            Console.WriteLine();
+            Console.WriteLine("Iteracje petla for: ");
+
+            for (int j = 0; j <= it+1; j++)
+            {
+                Console.WriteLine(j); ;
+            }
+            Console.ReadKey();
+        }
+    }
+}
