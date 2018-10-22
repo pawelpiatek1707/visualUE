95a9f3420414e42fc1df16780119f5673622c4ee
Paweł <pawelpiatek98@gmail.com>
2018-10-22 16:43:17 +0200

    bmi kalkulator

---
+++ bmi/bmi/Bmi.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bmi
+{
+    class Bmi
+    {
+        
+        float weight, height,bmi;
+        public Bmi(float weight, float height)
+        {
+           
+            this.weight = weight;
+            this.height = height;
+           
+
+        }
+      
+
+       
+
+       public float countBmi()
+        {
+             
+            return bmi = this.weight / (this.height * this.height);
+           
+        }
+        public void checkBmi()
+        {
+            if(bmi< 18.5f)
+            {
+                Console.WriteLine("Masz niedowage: ");
+            }
+            else if(bmi>=18.5f && bmi < 25)
+            {
+                Console.WriteLine("Twoje Bmi jest prawidlowe");
+            }
+            else
+            {
+                Console.WriteLine("Masz nadwage. Lepiej coś z tym zrob!!!!!!");
+            }
+        }
+    }
+}
---
+++ bmi/bmi/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bmi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("| SUPER PROGRAM DO LICZENIA BMI !!! |");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Podaj wage w kilogramach: ");
+            float weight = float.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj wzrost w metrach: ");
+            float height = float.Parse(Console.ReadLine());
+            Bmi bmi1 = new Bmi(weight,height);
+            Console.Clear();
+            float exe1 = bmi1.countBmi();
+            Console.WriteLine("-------------------");
+            Console.WriteLine("twoje bmi: " + exe1);
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Jeśli chcesz sprawdzić czy twoje Bmi jest prawidłowe wpisz 'BMI': ");
+            string check = Console.ReadLine();
+            if(check == "BMI" || check =="bmi")
+            {
+                Console.Clear();
+                bmi1.checkBmi();
+
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
