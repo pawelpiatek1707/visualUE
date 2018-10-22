d663d93b2bd512143b8146ded6c21a279cbaa689
Paweł <pawelpiatek98@gmail.com>
2018-10-22 18:18:11 +0200

    kalkulator

---
+++ kalkulator/kalkulator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kalkulator
+{
+    class Program
+    {
+        static void sum(float a, float b,  string choice)
+        {
+            if (choice == "+")
+            {
+                Console.WriteLine("Wynik dodawania: " + (a+b));
+            }
+            else if(choice == "-")
+            {
+                Console.WriteLine("Wynik odejmowania: " + (a - b));
+            }
+            else if (choice == "*")
+            {
+                Console.WriteLine("Wynik mnożenia: " + (a * b));
+            }
+            else if (choice == "/")
+            {
+                Console.WriteLine("Wynik dzielenia: " + (a / b));
+            }
+           
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kalkuator");
+            Console.WriteLine("---------");
+            Console.WriteLine("Podaj pierwsza liczbe: ");
+            float a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj druga liczbe: ");
+            float b = float.Parse(Console.ReadLine());
+            string choice;
+            do
+            {
+                Console.WriteLine("podaj symbol dzialania: ");
+                 choice = Console.ReadLine();
+            } while (choice != "+" && choice != "-" && choice != "*" && choice != "/");
+            sum(a, b, choice);
+            Console.ReadKey();
+        }
+        
+    }
+}
