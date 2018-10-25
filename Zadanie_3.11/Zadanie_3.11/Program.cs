e70cc88d6577a80bc7c9b5db59ea9cf422dd311f
Paweł <pawelpiatek98@gmail.com>
2018-10-25 20:57:30 +0200

    zadanie 3.11

---
+++ Zadanie_3.11/Zadanie_3.11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_3._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadanie 3.11");
+            int a = 1;
+            int b = 2;
+            int step = 0;
+            while(a < 100){
+                a = a + b;
+                b++;
+                step++;
+            }
+            //Console.WriteLine(a);
+            Console.WriteLine(step-1);
+            Console.ReadKey();
+        }
+    }
+}
