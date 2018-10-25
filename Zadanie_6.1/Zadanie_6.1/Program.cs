438e63d3415ae6ef94c059f4a7dd4a8d8ba40d6e
Paweł <pawelpiatek98@gmail.com>
2018-10-25 20:21:00 +0200

    silnia

---
+++ Zadanie_6.1/Zadanie_6.1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program obliczajacy silnie");
+            Console.WriteLine("Wprowadz silnie: ");
+            int  a= int.Parse(Console.ReadLine());
+            int silnia =1;
+            if (a >= 0)
+            {
+
+
+                if (a == 0 || a == 1)
+                {
+                    silnia = 1;
+                    Console.WriteLine("Silnia: " + silnia);
+                }
+                else
+                {
+                    
+                    for(int i = 1; i <= a;i++)
+                    {
+                        
+                        silnia = silnia *i;
+                       
+                    
+                    }
+                    Console.WriteLine("Silnia: "+ silnia);
+                }
+            }
+            else
+            {
+
+                Console.WriteLine("Silnia musi byc dodatnia: ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
