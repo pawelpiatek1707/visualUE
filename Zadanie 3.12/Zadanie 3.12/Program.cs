6e6ac051bf2715f57b9b598f2b83f3641435cd10
Paweł <pawelpiatek98@gmail.com>
2018-10-25 21:01:57 +0200

    Zadanie 3.12

---
+++ Zadanie 3.12/Zadanie 3.12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_3._12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sumowanie liczb wprowadzonnych przez użytkownika do czasu gdy nie wprowadzi on 0: ");
+            int a;
+            int suma=0;
+            do
+            {
+                Console.WriteLine("Wprowadz liczby: ");
+                a = int.Parse(Console.ReadLine());
+                suma += a;
+            } while (a != 0);
+            Console.WriteLine("suma: "+suma);
+            Console.ReadKey();
+            
+        }
+    }
+}
