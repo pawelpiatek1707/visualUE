b8b336b2edfc3fc443fb4c7f8c4fd1e68ab15f86
Paweł <pawelpiatek98@gmail.com>
2018-10-30 14:56:06 +0100

    zadanie 4.1 i 7.4

---
+++ arrays/arrays/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Zadanie 4.1 i 7.4
+            Console.WriteLine("Podaj liczbe elementow tablicy: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr1 = new int[n];
+            Console.WriteLine("Podaj elementy tablicy: ");
+            int a;
+            for(int i = 0; i < n; i++)
+            {
+                a = int.Parse(Console.ReadLine());
+                arr1[i] = a;
+            }
+            Console.WriteLine("------------------");
+            Console.WriteLine("Elementy tablicy: ");
+           
+            for(int i = 0; i<n; i++)
+            {
+                Console.WriteLine(arr1[i]);
+            }
+            Console.ReadKey();
+
+          
+        }
+    }
+}
