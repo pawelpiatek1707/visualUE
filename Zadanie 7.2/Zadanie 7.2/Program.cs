16524c748bbc1bb002c85e5e2d2bbcabfd6daee3
Paweł <pawelpiatek98@gmail.com>
2018-10-30 15:11:37 +0100

    Zadanie 7.2

---
+++ Zadanie 7.2/Zadanie 7.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab1 = new int[10] { 1, 2, -3, -4, 5, -6, 7, 8, -9,0 };
+            int[] tab2 = new int[10];
+
+            Console.WriteLine("Tablica 1: ");
+            for(int i = 0; i<10; i++)
+            {
+                Console.WriteLine(tab1[i]);
+            }
+            Console.WriteLine("Tablica 2: ");
+            for(int i =0; i<10; i++)
+            {
+                if (tab1[i] > 0)
+                {
+                    tab2[i] = tab1[i];
+                    Console.WriteLine(tab2[i]);
+
+                }
+             
+                
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
