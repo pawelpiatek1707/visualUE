3e153ea44ab71c8902b2fab4a3f6368bf3ba0c4e
Paweł <pawelpiatek98@gmail.com>
2018-11-13 11:10:39 +0100

    Zadanie 10.4

---
+++ Zadanie 10.4/Zadanie 10.4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_10._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program sprawdzający czy podany rok jest rokiem przestępnym");
+            Console.WriteLine("-----------------------------------------------------------");
+            Console.Write("Podaj rok: ");
+            int year = int.Parse(Console.ReadLine());
+            if ((year % 4 == 0) && (year % 100 != 0) && (year % 400 == 0))
+            {
+                Console.WriteLine("Jest rokiem przetępnym");
+            }
+            else
+                Console.WriteLine("Nie jjest rokiem przetępnym");
+            Console.ReadKey();
+        }
+       
+    }
+}
