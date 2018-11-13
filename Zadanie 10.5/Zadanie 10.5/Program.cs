76affde426593ba1e0db33044ba62619b8d3f6e8
Paweł <pawelpiatek98@gmail.com>
2018-11-13 11:15:17 +0100

    Zadanie 10.5

---
+++ Zadanie 10.5/Zadanie 10.5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_10._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sprawdzanie czy podane liczby tworzą trójke pitagorejską: ");
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("Podaj pierwsza liczbe: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj drugą liczbe: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj trzecią liczbe: ");
+            int c = int.Parse(Console.ReadLine());
+            if(a*a+b*b==c*c)
+                Console.WriteLine("Podane liczby tworzą trójke pitagorejską");
+            else
+                Console.WriteLine("Podane liczby nie tworzą trójki pitagorejskiej");
+            Console.ReadKey();
+        }
+    }
+}
