64b1aa59668fcd4777ddad418f7d2ded0e0cb9ba
Paweł <pawelpiatek98@gmail.com>
2018-11-13 11:02:00 +0100

    Zadanie 10.7

---
+++ Zadanie 10.7/Zadanie 10.7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_10._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekst = "Ala Ela Karol Marek Tadeusz";
+            Console.WriteLine(tekst);
+            string tekst2 = tekst.Replace(" ", ", ");
+            Console.WriteLine(tekst2);
+            Console.ReadKey();
+
+        }
+    }
+}
