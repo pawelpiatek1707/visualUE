a79ca9c936d54c79e1209fd80727f092d525196a
Paweł <pawelpiatek98@gmail.com>
2018-11-27 11:04:59 +0100

    koniec

---
+++ Zadniae 13.2/Zadniae 13.2/Kwadrat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadniae_13._2
+{
+    class Kwadrat
+    {
+        int bok;
+        ConsoleColor kolor;
+        string a;
+        public Kwadrat(int bok1, ConsoleColor kolor1,string a1)
+        {
+            bok = bok1;
+            kolor = kolor1;
+            a = a1;
+
+        }
+        public void  rysuj()
+        {
+            Console.ForegroundColor = kolor;
+            for(int i =0;i<bok;i++)
+            {
+                for(int j=0;j<bok;j++)
+                {
+                    Console.Write(a+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Zadniae 13.2/Zadniae 13.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadniae_13._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj bok kwadratu: ");
+            int bok = int.Parse(Console.ReadLine());
+            Console.Write("Podaj znak: ");
+            string a = Console.ReadLine();
+            Kwadrat k1 = new Kwadrat(bok, ConsoleColor.Cyan, a);
+            k1.rysuj();
+            Console.ReadKey();
+        }
+    }
+}
