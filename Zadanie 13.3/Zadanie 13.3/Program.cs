cd26386a05ec7573413cc3b578c4bdb126ecf5ae
Paweł <pawelpiatek98@gmail.com>
2018-11-27 11:18:14 +0100

    koniec

---
+++ Zadanie 13.3/Zadanie 13.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_13._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Samochód s1 = new Samochód("BMW", 666);
+            s1.startSilnika();
+            s1.jedzie(350);
+            s1.stop();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Zadanie 13.3/Zadanie 13.3/Samochód.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_13._3
+{
+    class Samochód
+    {
+        string model;
+        int moc;
+        public Samochód(string nazwa, int moc1)
+        {
+            model = nazwa;
+            moc = moc1;
+        }
+        public void startSilnika()
+        {
+            Console.WriteLine("Silnik uruchomiony");
+        }
+        public void jedzie(int predkosc)
+        {
+            Console.WriteLine("Samochód jedzie  "+predkosc+"mk/h");
+        }
+        public void stop()
+        {
+            Console.WriteLine("Samochód zatrzymany. Silnik wyłączon");
+        }
+    }
+}
