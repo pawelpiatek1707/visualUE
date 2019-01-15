aef6879bd2877152ff34956b5234122844d4ac83
Paweł <pawelpiatek98@gmail.com>
2019-01-15 13:56:46 +0100

    koniec

---
+++ Zadanie 16.1/Zadanie 16.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_16._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Samochod samochod1 = new Samochod("betka", "beemka", 3, 3.0);
+            Console.WriteLine("Podaj wartosci (ilosc paliwa, ilosc kilometrow, trasa, cena paliwa)");
+            Console.Write("Podaj ilosc paliwa: ");
+            double iloscPaliwa = double.Parse(Console.ReadLine());
+            Console.Write("Podaj ilosc kilometrow: ");
+            double iloscKolometrow = double.Parse(Console.ReadLine());
+            Console.Write("Podaj trase: ");
+            double trasa = double.Parse(Console.ReadLine());
+            Console.Write("Podaj cene paliwa: ");
+            double cenaPaliwa = double.Parse(Console.ReadLine());
+            samochod1.Info(iloscPaliwa,iloscKolometrow,trasa,cenaPaliwa);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zadanie 16.1/Zadanie 16.1/Samochod.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_16._1
+{
+    class Samochod
+    {
+        private string marka;
+        public string Marka
+        {
+            get { return marka; }
+            set { marka = value; }
+        }
+        private string model;
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        private int iloscDrzwi;
+        public int IloscDrzwi
+        {
+            get { return iloscDrzwi; }
+            set { iloscDrzwi = value; }
+        }
+        private double pojemnoscSilnika;
+        public double PojemnoscSilnika
+        {
+            get { return pojemnoscSilnika; }
+            set { pojemnoscSilnika = value; }
+        }
+        private double ObliczSrednieSpalanie(double iloscPaliwa, double iloscKilometrow)
+        {
+            double srednieSpalanie = iloscPaliwa/iloscKilometrow*100;
+            return srednieSpalanie;
+        }
+
+        private double Spalanie(double trasa, double iloscPaliwa, double iloscKilometrow)
+        {
+
+            double srednieSpalanie = ObliczSrednieSpalanie(iloscPaliwa, iloscKilometrow);
+            double srednieSpalanieWynik = srednieSpalanie * trasa/100d;
+            return srednieSpalanieWynik;
+
+            
+        }
+        public double ObliczKosztPrzejazdu(double trasa, double cenaPaliwa, double iloscPaliwa, double iloscKilometrow)
+        {
+            double spalanie = Spalanie(trasa, iloscPaliwa, iloscKilometrow);
+            double spalanieWynik = spalanie * cenaPaliwa;
+            return spalanieWynik;
+        }
+
+        public void Info(double iloscPaliwa, double iloscKilometrow, double trasa, double cenaPaliwa)
+        {
+            Console.WriteLine("Unformacje na temat samochodu");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Marka: " + Marka);
+            Console.WriteLine("Model: " +Model);
+            Console.WriteLine("Ilosć drzwi: "+IloscDrzwi);
+            Console.WriteLine("Pojemnosc silnika: "+ PojemnoscSilnika);
+            Console.WriteLine("Srednie spalanie: " + ObliczSrednieSpalanie(iloscPaliwa,iloscKilometrow)+"l");
+            Console.WriteLine("Spalanie na trasie: "+Spalanie(trasa,iloscPaliwa,iloscKilometrow));
+            Console.WriteLine("Koszty przejazdu: {0:c}", ObliczKosztPrzejazdu(trasa,cenaPaliwa,iloscPaliwa,iloscKilometrow));
+
+        }
+
+        public Samochod(string marka1,string model1, int ilosc, double pojemnosc)
+        {
+            
+            marka = marka1;
+            model = model1;
+            iloscDrzwi = ilosc;
+            pojemnoscSilnika = pojemnosc;
+        }
+     
+    }
+}
